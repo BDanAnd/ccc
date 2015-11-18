27e5247806da93f4ef36d4628ec378b7ff1349f9
BDA <bogomolov.danila@gmail.com>
2015-11-18 15:49:36 +0300

    fix tabs

--- src/bitvector.cpp
+++ src/bitvector.cpp
@@ -2,57 +2,57 @@
 
 bitvector& bitvector::operator=(const bitvector& other)
 {
-	if (this != &other)
-		bits = other.bits;
-	return *this;
+    if (this != &other)
+        bits = other.bits;
+    return *this;
 }
 
 const bitvector bitvector::op(const bitvector& left, const bitvector& right, function<bool(bool, bool)> fn)
 {
-	vector<bool> tmp;
-	vector<bool>::const_iterator a = left.bits.begin(), b = right.bits.begin();
-	while (a != left.bits.end() && b != right.bits.end()) {
-		tmp.push_back(fn(*a, *b));
-		++a;
-		++b;
-	}
-	return bitvector(tmp);
+    vector<bool> tmp;
+    vector<bool>::const_iterator a = left.bits.begin(), b = right.bits.begin();
+    while (a != left.bits.end() && b != right.bits.end()) {
+        tmp.push_back(fn(*a, *b));
+        ++a;
+        ++b;
+    }
+    return bitvector(tmp);
 }
 
 vector<bool>::reference bitvector::operator[](int i) {
-	return bits[i];
+    return bits[i];
 }
 
 vector<bool>::const_reference bitvector::operator[](int i) const {
-	return bits[i];
+    return bits[i];
 }
 
 const bitvector operator+(const bitvector& left, const bitvector& right)
 {
-	return bitvector::op(left, right, [](bool a, bool b){return a | b;});
+    return bitvector::op(left, right, [](bool a, bool b){return a | b;});
 }
 
 const bitvector operator-(const bitvector& left, const bitvector& right)
 {
-	return bitvector::op(left, right, [](bool a, bool b){return a & ~b;});
+    return bitvector::op(left, right, [](bool a, bool b){return a & ~b;});
 }
 
 const bitvector operator*(const bitvector& left, const bitvector& right)
 {
-	return bitvector::op(left, right, [](bool a, bool b){return a & b;});
+    return bitvector::op(left, right, [](bool a, bool b){return a & b;});
 }
 
 bool operator==(const bitvector& left, const bitvector& right)
 {
-	return left.bits == right.bits;
+    return left.bits == right.bits;
 }
 
 ostream& operator<<(ostream& os, const bitvector& d)
 {
-	//bit mask
-	for (auto it = d.bits.begin(), e = d.bits.end(); it != e; ++it)
-		os << (int)*it;
-	return os;
+    //bit mask
+    for (auto it = d.bits.begin(), e = d.bits.end(); it != e; ++it)
+        os << (int)*it;
+    return os;
 }
 
 bitvector::operator vector<int>()
